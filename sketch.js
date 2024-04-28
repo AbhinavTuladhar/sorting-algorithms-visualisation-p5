4a977bd3f289984ab05164a2ef28e1899d2936b9
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2024-04-28 18:02:23 +0545

    add radix for radix sort

--- sketch.js
+++ sketch.js
@@ -7,19 +7,30 @@ let barCount = Math.floor(width / barWidth)
 
 /** @type {Array<{value: number, colour: p5.Color}>} */
 let array
+
+// All the select tags
 let mySelect
 let barsSelect
-let selectedSort
+let radixSelect
+
+// 'state' variables
+/** * @type {'Regular' | 'Pyramid'} */
 let drawingType = 'Regular'
+/**  @type {string} */
+let selectedSort
+/**  @type {string} */
+let selectedRadix
 
 // alert('Use landscape mode for best results!')
 
 function setup() {
   mySelect = createSelect()
   barsSelect = createSelect()
+  radixSelect = createSelect()
 
   mySelect.position(windowWidth / 2 - 75, 10)
   barsSelect.position(windowWidth / 2 + 75, 10)
+  radixSelect.position(-100, -100)
   mySelect.addClass('selector')
 
   mySelect.changed(() => {
@@ -31,6 +42,13 @@ function setup() {
     drawingType = barsSelect.value()
     resetAnimation()
   })
+
+  radixSelect.changed(() => {
+    selectedRadix = radixSelect.value()
+    resetAnimation()
+  })
+
+  const radixSelectOptions = [2, 3, 4, 5, 6, 7, 8, 9, 10, 16]
 
   const selectOptions = [
     '-Select a sort-',
@@ -53,6 +71,10 @@ function setup() {
   barsSelect.option('Regular')
   barsSelect.option('Pyramid')
 
+  radixSelectOptions.forEach((option) => {
+    radixSelect.option(option.toString())
+  })
+
   createCanvas(windowWidth, windowHeight)
 
   array = makeArray(increment, barCount)
@@ -63,51 +85,61 @@ function resetAnimation() {
   switch (selectedSort) {
     case 'Bubble Sort':
       barWidth = sortingConfig['Bubble Sort']
+      radixSelect.position(-100, -100)
       updateAndCreateArray()
       bubbleSort(array)
       break
     case 'Quick Sort':
       barWidth = sortingConfig['Quick Sort']
+      radixSelect.position(-100, -100)
       updateAndCreateArray()
       quickSort(array, 0, array.length - 1)
       break
     case 'Heap Sort':
       barWidth = sortingConfig['Heap Sort']
+      radixSelect.position(-100, -100)
       updateAndCreateArray()
       heapSort(array)
       break
     case 'LSD Radix Sort':
       barWidth = sortingConfig['LSD Radix Sort']
       updateAndCreateArray()
-      radixSort(array, 8)
+      radixSelect.position(windowWidth / 2, 30)
+      radixSort(array, +selectedRadix)
       break
     case 'Shell Sort':
       barWidth = sortingConfig['Shell Sort']
+      radixSelect.position(-100, -100)
       updateAndCreateArray()
       shellSort(array)
       break
     case 'Selection Sort':
       barWidth = sortingConfig['Selection Sort']
+      radixSelect.position(-100, -100)
       updateAndCreateArray()
       selectionSort(array)
       break
     case 'Insertion Sort':
       barWidth = sortingConfig['Insertion Sort']
+      radixSelect.position(-100, -100)
       updateAndCreateArray()
       insertionSort(array)
       break
     case 'Cocktail Shaker Sort':
       barWidth = sortingConfig['Cocktail Shaker Sort']
+      radixSelect.position(-100, -100)
       updateAndCreateArray()
       cocktailSort(array)
       break
     case 'Odd-even Sort':
       barWidth = sortingConfig['Odd-even Sort']
+      radixSelect.position(-100, -100)
       updateAndCreateArray()
       oddEvenSort(array)
       break
     case 'Merge Sort':
       barWidth = 5
+      radixSelect.position(-100, -100)
       updateAndCreateArray()
       mergeSort(array)
       break
